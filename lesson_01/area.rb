825315041dfda375178b8ea7f22db45b161653b6
Rick-ROR <Rick.ROR@ya.ru>
2019-01-22 21:19:38 +0000

    fix lesson_01

--- lesson_01/area.rb
+++ lesson_01/area.rb
@@ -2,10 +2,10 @@
 # coding: utf-8
 
 print "Введите основание треугольника: "
-a = gets.chomp.to_i
+a = gets.chomp.to_f
 
 print "Введите высоту треугольника: "
-h = gets.chomp.to_i
+h = gets.chomp.to_f
 
 area = 1 / 2.0 * a * h
 puts "Площадь данного треугольника равна #{area}."
--- lesson_01/quadratic.rb
+++ lesson_01/quadratic.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby -w
 # coding: utf-8
-print "Ввведите коэффициенты A B C через пробел: "
+print 'Ввведите коэффициенты A B C через пробел: '
 abc = gets.chomp.split(" ")
 
 a, b, c = abc.map{ |x| x.to_i }
@@ -8,13 +8,15 @@ a, b, c = abc.map{ |x| x.to_i }
 dis = b**2 - 4 * a * c
 puts "Дискриминант равен #{dis};"
 
+sqrt_dis = Math.sqrt(dis)
+
 if dis > 0
-    x1 = ( -b - Math.sqrt(dis) ) / ( 2 * a )
-    x2 = ( -b + Math.sqrt(dis) ) / ( 2 * a )
-    puts "Дискриминант больше 0, x1 = #{x1}, x2 = #{x2}"
+	x1 = (-b - sqrt_dis) / (2 * a)
+	x2 = (-b + sqrt_dis) / (2 * a)
+	puts "Дискриминант больше 0, x1 = #{x1}, x2 = #{x2}"
 elsif dis == 0
-    x =  -b / ( 2 * a )
-    puts "Дискриминант равен 0, x = #{x}"
+	x =  -b / (2 * a)
+	puts "Дискриминант равен 0, x = #{x}"
 else
-    puts 'Корней нет.'
+	puts 'Корней нет.'
 end
