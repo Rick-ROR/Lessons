858512cddc296d094fa06c261ad41b7c43c568cf
Rick-ROR <Rick.ROR@ya.ru>
2019-01-28 19:46:02 +0000

    fix & class/file

--- lesson_04/train.rb
+++ lesson_04/train.rb
@@ -1,106 +1,6 @@
 #!/usr/bin/ruby -w
 # coding: utf-8
 
-class Station
-  attr_reader :name
-
-  def initialize(name)
-    @name = name
-    @freight_trains = {}
-    @passenger_trains = {}
-  end
-
-# поезд прибывает на станцию
-  def arrival_train(train)
-    if train.type == :freight
-      @freight_trains[train.number] = train
-    else
-      @passenger_trains[train.number] = train
-    end
-  end
-
-# поезд отбывает со станции
-  def departure_trains(train)
-    if train.type == :freight
-      @freight_trains.delete(train.number)
-    else
-      @passenger_trains.delete(train.number)
-    end
-  end
-
-# показывает сколько поездов по типам и список для каждого
-  def show_trains_by_types
-    puts "Всего поездов на станции:"
-    puts "#{@freight_trains.keys.size} Грузовых: #{@freight_trains.keys.join(", ")}"
-    puts "#{@passenger_trains.keys.size} Пассажирских: #{@passenger_trains.keys.join(", ")}"
-  end
-
-# список поездов без разделения по типу
-  def show_trains_all
-    trains = @freight_trains.merge(@passenger_trains)
-    puts "Всего поездов на станции: #{trains.keys.size}"
-    puts "#{trains.keys.join(", ")}"
-  end
-end
-
-class Route
-  attr_reader :route_hsh
-
-  def initialize(first, last)
-    @route_hsh = {first.name => first, last.name => last}
-  end
-
-  # можно добавить станцию указав её номер на маршруте
-  def add_station(station, number)
-    # если такая станцию уже есть
-    if @route_hsh[station.name]
-      puts "Станция #{station.name} уже находится на этом маршруте!"
-      return
-    end
-
-    # нельзя переписать начальную станцию и конечную
-    if number.zero?
-      puts "Начальную станцию нельзя изменить!"
-      return
-    elsif number >= @route_hsh.keys.size
-      puts "Конечную станцию нельзя изменить!"
-      return
-    end
-
-    # переписываем маршрут с учётом новой станции
-    # возможно можно сделать красивее или есть что-то типа insert для хеша
-    name_stations = @route_hsh.keys.insert(number, station.name)
-    new_route = {}
-    name_stations.each do |name_station|
-      new_route[name_station] = @route_hsh[name_station]
-    end
-    new_route[station.name] = station
-    @route_hsh = new_route
-  end
-
-  # удаляем станцию из маршрута
-  def del_station(station)
-    # получаем начальную и конечную станции
-    route_ends = @route_hsh.keys.values_at(0,-1)
-    # проверяем что станция есть на маршруте и что она не начальная или конечная
-    if !@route_hsh.key?(station.name)
-      puts "Станция #{station.name} не существует на данном маршруте!"
-    elsif route_ends.include?(station.name)
-      puts "Это начальная или конечная станция маршрута, их нельзя изменять!"
-    else
-      @route_hsh.delete(station.name)
-      puts "Станция #{station.name} удалена с данного маршрута."
-    end
-  end
-
-  # выводит список станций на маршруте с нумерацией
-  def print_stations
-    puts "Данный маршрут состоит из следующих станций:"
-    @route_hsh.keys.each_with_index {|station, index| print "= #{index}# #{station} = "}
-    puts
-  end
-end
-
 class Train
   attr_reader :number, :type, :wagons, :speed, :current_station
 
@@ -127,9 +27,9 @@ class Train
   end
 
 # вовзращает true если 0
-def stopped?
-  @speed == 0
-end
+  def stopped?
+    @speed == 0
+  end
 
 # останаливаем поезд
   def speed_stop
